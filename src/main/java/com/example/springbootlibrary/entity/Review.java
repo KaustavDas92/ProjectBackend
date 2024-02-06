17a3237c3819371a96c3b227965bee308b682d57
KaustavDas92 <kaustavdas192@gmail.com>
2024-02-07 02:15:44 +0530

    3 pages done

---
+++ src/main/java/com/example/springbootlibrary/entity/Review.java
@@ -0,0 +1,32 @@
+package com.example.springbootlibrary.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Review")
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "rating")
+    private double rating;
+
+    @Column(name = "book_id")
+    private long bookId;
+
+    @Column(name = "review_description")
+    private String reviewDescription;
+}
